e2b3615a7348fb4c035886779cdde4d8a18fbb8b
Douglas <douglasso1206@gmail.com>
2024-11-29 20:46:17 -0300

    adicioana a função de enviar um e-mail com os resultados

--- functions.py
+++ functions.py
@@ -1,3 +1,9 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
 def break_lines(texto, tamanho_maximo):
     palavras = texto.split()
 
@@ -26,3 +32,29 @@ def veriry_keywords(keywords, title, text):
         if word in title or word in text:
             return True
     return False
+
+
+def send_email(subject, body, to_email):
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    from_email = os.getenv("EMAIL_USER")
+    from_password = os.getenv("EMAIL_PASSWORD")
+
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(from_email, from_password)
+        text = msg.as_string()
+        server.sendmail(from_email, to_email, text)
+        print("E-mail enviado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
+    finally:
+        server.quit()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 from time import sleep
 from selenium import webdriver
+from functions import send_email
 from functions import break_lines
 from functions import veriry_keywords
 from selenium.webdriver.common.by import By
@@ -17,54 +19,66 @@ driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()), options=chrome_options
 )
 
-driver.get("https://aguasdorio.com.br/comunicados/")
 
-try:
-    sleep(3)
-    button = driver.find_element(By.XPATH, "//button[text()='Mais Comunicados']")
-    button.click()
-    sleep(3)
+def main():
+    driver.get("https://aguasdorio.com.br/comunicados/")
 
-    div_elements = driver.find_elements(By.XPATH, '//div[@data-component="card-news"]')
+    try:
+        sleep(3)
+        button = driver.find_element(By.XPATH, "//button[text()='Mais Comunicados']")
+        button.click()
+        sleep(3)
 
-    for div_element in div_elements:
-        try:
-            h5_element = div_element.find_element(
-                By.XPATH, './/h5[contains(@class, "card-title")]'
-            )
-            title_text = h5_element.text
+        div_elements = driver.find_elements(
+            By.XPATH, '//div[@data-component="card-news"]'
+        )
 
-            span_element = div_element.find_element(
-                By.XPATH, './/span[contains(@class, "date")]'
-            )
-            date_text = span_element.text
+        for div_element in div_elements:
+            try:
+                h5_element = div_element.find_element(
+                    By.XPATH, './/h5[contains(@class, "card-title")]'
+                )
+                title_text = h5_element.text
 
-            paragraph_elements = div_element.find_elements(
-                By.XPATH, './/div[contains(@class, "card-text")]//p'
-            )
+                span_element = div_element.find_element(
+                    By.XPATH, './/span[contains(@class, "date")]'
+                )
+                date_text = span_element.text
 
-            paragraph_texts = [p.text for p in paragraph_elements]
-            full_text = break_lines((" ".join(paragraph_texts)), 150)
-
-            keywords = ["Grande Tijuca" "Tijuca", "Vila Isabel", "Capital"]
-
-            match = veriry_keywords(keywords, title_text, full_text)
-
-            if not match:
-                continue
-
-            print(f"Título: {title_text}")
-            print(f"Data: {date_text}")
-            print(f"Texto: {full_text}")
-
-            with open("last-results.txt", "a", encoding="utf-8") as file:
-                file.write(
-                    f"Título: {title_text}\n\nData: {date_text}\n\nTexto: {full_text}\n\n---------------------------\n\n"
+                paragraph_elements = div_element.find_elements(
+                    By.XPATH, './/div[contains(@class, "card-text")]//p'
                 )
 
-            print("-----")
-        except Exception as e:
-            print(f"Erro ao buscar elementos dentro da div: {e}")
+                paragraph_texts = [p.text for p in paragraph_elements]
+                full_text = break_lines((" ".join(paragraph_texts)), 150)
 
-except Exception as e:
-    print(f"Elemento não encontrado ou ocorreu um erro: {e}")
+                keywords = ["Grande Tijuca" "Tijuca", "Vila Isabel", "Capital"]
+
+                match = veriry_keywords(keywords, title_text, full_text)
+
+                if not match:
+                    continue
+
+                print(f"Título: {title_text}")
+                print(f"Data: {date_text}")
+                print(f"Texto: {full_text}")
+
+                body = f"Título: {title_text}\n\nData: {date_text}\n\nTexto: {full_text}\n\n---------------------------\n\n"
+
+                with open("last-results.txt", "a", encoding="utf-8") as file:
+                    file.write(body)
+
+                print("-----")
+
+                send_email(
+                    "Comunicados Águas do Rio", body, os.getenv("EMAIL_RECEIVER")
+                )
+            except Exception as e:
+                print(f"Erro ao buscar elementos dentro da div: {e}")
+
+    except Exception as e:
+        print(f"Elemento não encontrado ou ocorreu um erro: {e}")
+
+
+if __name__ == "__main__":
+    main()
