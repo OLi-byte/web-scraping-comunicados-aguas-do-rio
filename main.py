d3960eca9e402038119a497955228d5d0a4ffae4
Douglas <douglasso1206@gmail.com>
2024-12-01 20:50:33 -0300

    update webdriver arguments

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-usb-discovery")
 
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()), options=chrome_options
